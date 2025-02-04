4fe44550438f44ccdb1381e4ba7e7386572a3a15
djondozy <165369403+djondozy@users.noreply.github.com>
2025-02-05 00:38:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,5 @@
-import pygame
-from serpent import Serpent
-from nourriture import Nourriture
-import sys
+from game import Game
 
-#Initialiser pygame
-pygame.init()
-
-# Paramètres de la fenêtre
-WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Jeux du serpent")
-
-# Couleurs
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-
-# Initialisation du jeu
-serpent = Serpent()
-nourriture = Nourriture()
-running = True
-clock = pygame.time.Clock()
-
-
-screen.blit(pygame.image.load("welcome.png"), [0, 0])
-pygame.display.flip()
-pygame.time.wait(3000)
-
-
-running = True
-while running:
-    screen.fill(BLACK)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.flip()
-
+if __name__ == "__main__":
+    game = Game()
+    game.run()
